e1e269d57223389cc2a030de81b467b56d42cf81
EC2 Default User <ec2-user@ip-10-0-2-25.ec2.internal>
2025-10-29 07:59:22 +0000

    estoy en tus manos dios

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,64 +1,51 @@
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda_exec_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
+
+data "aws_iam_role" "labrole" {
+  name = "labrole"
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
 
 locals {
   db_env = {
     DB_HOST = "nexa-db-instance.cl1mlfaminhs.us-east-1.rds.amazonaws.com:9876"
     DB_NAME = "estudiante"
-    DB_USER = "postgres"
-    DB_PASS = "TU_CONTRASEÑA"
+    DB_USER = "nexa_admin"
+    DB_PASS = "tjaUWyY7c+wXXl6r"
     DB_PORT = "5432"
   }
 }
 
 # Reutilizamos el mismo rol y variables para cada lambda
 resource "aws_lambda_function" "add_student" {
-  filename         = "add_student.zip"
+  filename         = "añadir_estudiante.zip"
   function_name    = "add_student"
-  role             = aws_iam_role.lambda_exec_role.arn
+  role             = data.aws_iam_role.labrole.arn
   handler          = "main.lambda_handler"
   runtime          = "python3.12"
-  source_code_hash = filebase64sha256("add_student.zip")
+  source_code_hash = filebase64sha256("añadir_estudiante.zip")
   environment {
     variables = local.db_env
   }
 }
 
 resource "aws_lambda_function" "list_students" {
-  filename         = "list_students.zip"
+  filename         = "listar_estudiantes.zip"
   function_name    = "list_students"
-  role             = aws_iam_role.lambda_exec_role.arn
+  role             = data.aws_iam_role.labrole.arn
   handler          = "main.lambda_handler"
   runtime          = "python3.12"
-  source_code_hash = filebase64sha256("list_students.zip")
+  source_code_hash = filebase64sha256("listar_estudiantes.zip")
   environment {
     variables = local.db_env
   }
 }
 
 resource "aws_lambda_function" "delete_student" {
-  filename         = "delete_student.zip"
+  filename         = "eliminar_estudiante.zip"
   function_name    = "delete_student"
-  role             = aws_iam_role.lambda_exec_role.arn
+  role             = data.aws_iam_role.labrole.arn
   handler          = "main.lambda_handler"
   runtime          = "python3.12"
-  source_code_hash = filebase64sha256("delete_student.zip")
+  source_code_hash = filebase64sha256("eliminar_estudiante.zip")
   environment {
     variables = local.db_env
   }
